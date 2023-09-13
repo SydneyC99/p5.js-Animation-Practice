170036176092e50f23a0ee1c220c22cca9bb1757
SYDNEY COLVIN <SGC21E@FSU.EDU>
2023-09-13 11:32:44 -0400

    End of Class

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+let x = 200;
+let speed = 2;
+let diameter = 40;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(125);
+  background(225, 125, 0);
+  ellipse(x, height/2, 40);
+  x += speed;
+  if (x >= width - diameter/2 || x <= 0 + diameter/2) {
+    speed = speed * -1
+  }
 }
